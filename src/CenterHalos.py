ebbc9d6ef59bb5dd274c65175902a9b8f2aea093
Christian Poveda <cn.poveda.ruiz@gmail.com>
2016-04-09 11:50:21 -0500

    modified for bolshoi

--- src/CenterHalos.py
+++ src/CenterHalos.py
@@ -1,6 +1,7 @@
 import numpy as np, sys, os
 
-rho0 = (2048.0/1000.0)**3
+#rho0 = (2048.0/1000.0)**3 # For MDR1
+rho0 = (2048.0/250)**3 # For Bolshoi
 crit = 740.0
 
 path = sys.argv[1]
@@ -16,11 +17,13 @@ for i in range(filenum):
     filename = filenames[i]
     print('Working with file %d of %d' % (i,filenum))
 
-    data = np.loadtxt(path+'/'+filename, delimiter=",",skiprows=16,usecols=(2,3,4))
+    #data = np.loadtxt(path+'/'+filename, delimiter=",",skiprows=16,usecols=(2,3,4)) # For MDR1
+    data = np.loadtxt(path+'/'+filename) # For Bolshoi
     x,y,z = np.transpose(data)
     n_points = len(x)
     
-    file_id = int(filename.split('_')[1])
+    #file_id = int(filename.split('_')[1]) # For MDR1
+    file_id = int(filename.split('_')[1].split('.')[0]) # For Bolshoi 
     positions_name ='positions_'+str(file_id)+'.dat'
     potential_name ='potential_'+str(file_id)+'.dat'
 
